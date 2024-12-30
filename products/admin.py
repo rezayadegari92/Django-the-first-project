cba3a66036840febf35cd4e7889515b287594874
reza <yadegarireza50@googlemail.com>
2024-12-30 21:25:16 +0330

    some changes added

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from products.models import Product
 
 # Register your models here.
+class AdminProduct(admin.ModelAdmin):
+    list_display = ["product_name","product_price","category"]
+    
+
+
+admin.site.register(Product, AdminProduct)
--- products/models.py
+++ products/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     CATEGORY_CHOICES = [
-        ('OPTION1','Furniture'),
-        ('OPTION2','Electronic')
+        ('Furniture','Furniture'),
+        ('Electronic','Electronic'),
         ]
     product_name = models.CharField(max_length=30)
     product_price = models.DecimalField(max_digits=15, decimal_places=2)
     available = models.BooleanField(default=True)
-    category = models.CharField(max_length=20,choises=CATEGORY_CHOICES)
+    category = models.CharField(max_length=10,choices = CATEGORY_CHOICES,default='null')
 
     def __str__(self):
         return f"{self.product_name} - {self.product_price} - {self.category}"
