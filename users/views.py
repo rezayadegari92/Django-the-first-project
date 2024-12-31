6e4877ed17b3166cf64dce0235516f93b6bf3695
reza <yadegarireza50@googlemail.com>
2024-12-31 11:09:22 +0330

    users views modified

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import User
 # Create your views here.
+def users_show(request):
+    users = User.objects
+    return render(request, 'users.html',{'users':users})
