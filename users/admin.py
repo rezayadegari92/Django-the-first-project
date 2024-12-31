7bb5a24c0dc650606c69ad2462c2ce4cdf901f4c
reza <yadegarireza50@googlemail.com>
2024-12-31 10:37:57 +0330

    register admin users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from users.models import User
 # Register your models here.
+
+class AdminUser(admin.ModelAdmin):
+    list_display = ["user_name","email","phone_number"]
+
+admin.site.register(User, AdminUser)    
