5fa28e43d52b1587763c209cde3b46018379a5f9
reza <yadegarireza50@googlemail.com>
2024-12-31 10:51:09 +0330

    users models modified

--- users/models.py
+++ users/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.core.validators import RegexValidator
-phone_regex = RegexValidator( regex=r'^\+?1?\d{11}$', message="Phone number must be 11 digits ." )  
+
+phone_regex = RegexValidator(regex=r'^\+?1?\d{11}$', message="Phone number must be 11 digits .")  
 # Create your models here.
 class User(models.Model):
-    user_name = CharField(max_length=20)
+    user_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=70,blank=True,unique=True)
     phone_number = models.CharField(validators=[phone_regex], max_length=11)
 
