33fe53cde0d05d3af33793a7d7cfd42180c56651
reza <yadegarireza50@googlemail.com>
2025-01-06 19:58:24 +0330

    view in prod file modified

--- products/views.py
+++ products/views.py
@@ -4,4 +4,9 @@ from .models import Product
 # Create your views here.
 def products_show(request):
     products = Product.objects.filter(available=True)
-    return render(request, 'store.html',{'products':products})
+    return render(request, 'products.html',{'products':products})
+
+
+def product_detail(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'pro_detaile.html',{'product':product})    
