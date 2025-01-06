059a369712fb777eaa64b3d41798ee0ebcbcccb3
reza <yadegarireza50@googlemail.com>
2025-01-06 11:21:41 +0330

    something was chenged

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'users',
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
--- users/views.py
+++ users/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import User
 # Create your views here.
 def users_show(request):
-    users = User.objects
+    users = User.objects.all()
     return render(request, 'users.html',{'users':users})
